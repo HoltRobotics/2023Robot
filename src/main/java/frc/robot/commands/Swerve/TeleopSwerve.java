a070db89210ed03e911d99f31f0fe79982a31ba8
Nixis198 <gager198@gmail.com>
2023-01-08 11:59:10 -0500

    first commit

---
+++ src/main/java/frc/robot/commands/Swerve/TeleopSwerve.java
@@ -0,0 +1,50 @@
+package frc.robot.commands.Swerve;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.Swerve;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+
+public class TeleopSwerve extends CommandBase {    
+    private DoubleSupplier m_translation;
+    private DoubleSupplier m_strafe;
+    private DoubleSupplier m_rotation;
+    private BooleanSupplier m_fieldCentric;
+    private Swerve m_swerve;
+
+    public TeleopSwerve(DoubleSupplier translation, DoubleSupplier strafe, DoubleSupplier rotation, BooleanSupplier fieldCentric, Swerve s_Swerve) {
+        m_translation = translation;
+        m_strafe = strafe;
+        m_rotation = rotation;
+        m_fieldCentric = fieldCentric;
+        m_swerve = s_Swerve;
+        addRequirements(s_Swerve);
+    }
+
+    @Override
+    public void execute() {
+        /* Get Values, Deadband*/
+        double translationVal = MathUtil.applyDeadband(m_translation.getAsDouble(), Constants.stickDeadband);
+        double strafeVal = MathUtil.applyDeadband(m_strafe.getAsDouble(), Constants.stickDeadband);
+        double rotationVal = MathUtil.applyDeadband(m_rotation.getAsDouble(), Constants.stickDeadband);
+
+        /* Drive */
+        m_swerve.drive(
+            new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed), 
+            rotationVal * Constants.Swerve.maxAngularVelocity, 
+            m_fieldCentric.getAsBoolean(), 
+            true
+        );
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_swerve.stopDrive();
+    }
+}
