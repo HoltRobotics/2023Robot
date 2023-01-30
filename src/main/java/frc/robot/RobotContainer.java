3a362a9d11daf63e3195bfd88da9d12863586153
Nixis198 <gager198@gmail.com>
2023-01-29 19:27:36 -0500

    Added auton chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,10 @@ import com.pathplanner.lib.auto.SwerveAutoBuilder;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -38,7 +42,12 @@ public class RobotContainer {
     private final SwerveAutoBuilder m_autoBuilder;
     private final HashMap<String, Command> m_eventMap = new HashMap<>();
 
-    private final PathPlannerTrajectory m_path = PathPlanner.loadPath("Test Path", new PathConstraints(4, 3));
+    private final ShuffleboardTab m_tab = Shuffleboard.getTab("Main");
+    private final SendableChooser<PathPlannerTrajectory> m_autoChooser = new SendableChooser<>();
+
+    private final PathPlannerTrajectory m_testPath = PathPlanner.loadPath("Test Path", new PathConstraints(4, 3));
+    private final PathPlannerTrajectory m_transPath = PathPlanner.loadPath("Translation Path", new PathConstraints(4, 3));
+    private final PathPlannerTrajectory m_rotPath = PathPlanner.loadPath("Rotation Path", new PathConstraints(4, 3));
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
@@ -51,6 +60,11 @@ public class RobotContainer {
                 m_swerve
             )
         );
+
+        m_tab.add("Auton List", m_autoChooser).withPosition(0, 2).withSize(2, 1).withWidget(BuiltInWidgets.kComboBoxChooser);
+        m_autoChooser.setDefaultOption("Test Path", m_testPath);
+        m_autoChooser.addOption("Translation Path", m_transPath);
+        m_autoChooser.addOption("Rotation Path", m_rotPath);
 
         m_eventMap.put("test1", new PrintCommand("Test 1"));
         m_eventMap.put("test2", new PrintCommand("Test 2"));
@@ -97,7 +111,7 @@ public class RobotContainer {
      */
     public Command getAutonomousCommand() {
         // An ExampleCommand will run in autonomous
-        Command fullAuto = m_autoBuilder.fullAuto(m_path);
+        Command fullAuto = m_autoBuilder.fullAuto(m_autoChooser.getSelected());
         return fullAuto;
         // return new exampleAuto(m_swerve);
     }
