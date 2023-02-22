1aaf1efdfec0c938aefe79238682c9064e0154d9
Nixis198 <gager198@gmail.com>
2023-02-22 12:44:10 -0500

    Started Documentation

--- src/main/java/frc/robot/commands/Arm/DownArm.java
+++ src/main/java/frc/robot/commands/Arm/DownArm.java
@@ -12,44 +12,48 @@ public class DownArm extends CommandBase {
   private final Arm m_arm;
   private boolean m_pastLimit = false;
 
-  /** Creates a new Up. */
+  /**
+   * Command that forces the arm to rotate down.
+   * When the command ends, the new location will become the new setpoint.
+   * @param arm The Arm Subsystem
+   */
   public DownArm(Arm arm) {
     // Use addRequirements() here to declare subsystem dependencies.
-    m_arm = arm;
-    addRequirements(m_arm);
+    m_arm = arm; // Passes the given subsystem to the rest of the command.
+    addRequirements(m_arm); // Stops all other commands using the Arm subsystem.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_arm.disable();
+    m_arm.disable(); // Disables the PID controller so it won't fight back.
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_arm.down();
+    m_arm.down(); // Calls for the arm to run down.
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    if(m_pastLimit) {
-      m_arm.setAngle(ArmConstants.kMaxAngle);
+    if(m_pastLimit) { // Checks to see if the arm when past its max.
+      m_arm.setAngle(ArmConstants.kMaxAngle); // If it did, set it back to the max.
     } else{
-      m_arm.setAngle(m_arm.getAngle());
+      m_arm.setAngle(m_arm.getAngle()); // If it didn't set the PID setpoint to the new angle.
     }
-    m_arm.enable();
+    m_arm.enable(); // Re-enables the PID controller.
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(m_arm.getAngle() > ArmConstants.kMaxAngle) {
-      m_pastLimit = true;
-      return true;
+    if(m_arm.getAngle() > ArmConstants.kMaxAngle) { // Checks to see if the arm is past the max angle.
+      m_pastLimit = true; // If it is, tells the command that it went past the limit.
+      return true; // Ends the command.
     } else{
-      return false;
+      return false; // Let the command keep running.
     }
   }
 }
--- src/main/java/frc/robot/commands/Arm/SetAngle.java
+++ src/main/java/frc/robot/commands/Arm/SetAngle.java
@@ -11,15 +11,20 @@ public class SetAngle extends InstantCommand {
   private double m_angle;
   private Arm m_arm;
 
+  /**
+   * Command to set the PID angle of the Arm.
+   * @param angle Angle to set the arm too.
+   * @param arm The Arm Subsystem.
+   */
   public SetAngle(double angle, Arm arm) {
     // Use addRequirements() here to declare subsystem dependencies.
-    m_angle = angle;
-    m_arm = arm;
+    m_angle = angle; // Passes the given angle to the rest of the command.
+    m_arm = arm; // Passes the given subsystem to the rest of the command.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_arm.setAngle(m_angle);
+    m_arm.setAngle(m_angle); // Sets the PID angle to the given angle.
   }
 }
